ccdc29350fb9907f41fb576c37b0b391fd7e0142
Juan <jzapata@interplay.tech>
2024-01-05 15:24:19 -0500

    second commit

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -21,14 +21,14 @@ function TaskForm() {
       <h1 className="text-2xl font-extrabold m-3 text-white">Add task</h1>
       <form onSubmit={handleSubmit} className="bg-emerald-500 p-10 mb-5 rounded-3xl">
         <input
-          placeholder="Write your task!"
+          placeholder="Write your task here!"
           onChange={(e) => setTitle(e.target.value)}
           value={title}
           autoFocus={true}
           className="bg-slate-100 p-3 w-full mb-2"
         />
         <textarea
-          placeholder="Writhe the description"
+          placeholder="Describe your task in this box."
           onChange={(e) => setDescription(e.target.value)}
           value={description}
           className="bg-slate-100 p-3 w-full mb-2"
